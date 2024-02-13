e39df3d362b43b10fa563e9bfc34710ac05cdbec
caregor <64469336+caregor@users.noreply.github.com>
2024-02-13 21:54:14 +0500

    Task 2 is done;

--- src/main/java/ru/gb/hw/Main.java
+++ src/main/java/ru/gb/hw/Main.java
@@ -1,5 +1,7 @@
 package ru.gb.hw;
 
+import static ru.gb.hw.ComparatorArray.compareArrays;
+
 public class Main {
     public static void main(String[] args) {
         // Пример для Задания № 1
@@ -10,6 +12,21 @@ public class Main {
         System.out.println("Произведение: " + Calculator.multiply(x, y));
         System.out.println("Частное: " + Calculator.divide(x, y));
         System.out.println("Разность: " + Calculator.subtract(x, y));
+
+        // Примеры для Задания №2
+        Integer[] iArray1 = {1, 2, 3, 4, 5};
+        Integer[] iArray2 = {1, 2, 3, 4, 5};
+        Integer[] iArray3 = {1, 2, 3, 4, 6};
+
+        String[] sArray1 = {"apple", "banana", "orange"};
+        String[] sArray2 = {"apple", "banana", "orange"};
+        String[] sArray3 = {"apple", "banana", "grape"};
+
+        System.out.println("iArray1 and iArray2 equal: " + compareArrays(iArray1, iArray2));
+        System.out.println("iArray1 and iArray3 equal: " + compareArrays(iArray1, iArray3));
+
+        System.out.println("sArray1 and sArray2 equal: " + compareArrays(sArray1, sArray2));
+        System.out.println("sArray1 and sArray3 equal: " + compareArrays(sArray1, sArray3));
 
     }
 }
