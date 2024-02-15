ce0861501e72ae93b5904d191fcc27f36c2dae9a
caregor <64469336+caregor@users.noreply.github.com>
2024-02-15 09:47:15 +0500

    Refactor HW is done;

--- src/main/java/ru/gb/hw/ComparatorArray.java
+++ src/main/java/ru/gb/hw/ComparatorArray.java
@@ -1,17 +1,16 @@
 package ru.gb.hw;
-
+/*
+Напишите обобщенный метод compareArrays(), который принимает два массива и возвращает true, если они одинаковые,
+и false в противном случае. Массивы могут быть любого типа данных, но должны иметь одинаковую длину и содержать
+элементы одного типа.
+ */
 public class ComparatorArray {
-    public static <T> boolean compareArrays(T[] arr1, T[] arr2) {
+    public static <T, E> boolean compareArrays(T[] arr1, E[] arr2) {
         if (arr1.length != arr2.length) {
             return false;
         }
 
-        for (int i = 0; i < arr1.length; i++) {
-            if (!arr1[i].equals(arr2[i])) {
-                return false;
-            }
-        }
-        return true;
+        return arr1.getClass().getComponentType().equals(arr2.getClass().getComponentType());
     }
 
 }
--- src/main/java/ru/gb/hw/Main.java
+++ src/main/java/ru/gb/hw/Main.java
@@ -15,11 +15,11 @@ public class Main {
 
         // Примеры для Задания №2
         Integer[] iArray1 = {1, 2, 3, 4, 5};
-        Integer[] iArray2 = {1, 2, 3, 4, 5};
+        Integer[] iArray2 = {1, 2, 3, 4, 5, 6};
         Integer[] iArray3 = {1, 2, 3, 4, 6};
 
         String[] sArray1 = {"apple", "banana", "orange"};
-        String[] sArray2 = {"apple", "banana", "orange"};
+        String[] sArray2 = {"apple", "banana", "orange", "pineapple"};
         String[] sArray3 = {"apple", "banana", "grape"};
 
         System.out.println("iArray1 and iArray2 equal: " + compareArrays(iArray1, iArray2));
@@ -28,9 +28,14 @@ public class Main {
         System.out.println("sArray1 and sArray2 equal: " + compareArrays(sArray1, sArray2));
         System.out.println("sArray1 and sArray3 equal: " + compareArrays(sArray1, sArray3));
 
+        System.out.println("iArray1 and sArray1 equal: " + compareArrays(iArray1, sArray1));
+
         //Пример для Задания №3
         Pair<Integer, String> pair1 = new Pair<>(1, "one");
         Pair<Double, Character> pair2 = new Pair<>(1.15, 'a');
+
+        System.out.println("Pair 1 the first element: " + pair1.getFirst());
+        System.out.println("Pair 2 the second element: " + pair2.getSecond());
 
         System.out.println("Pair 1: " + pair1.toString());
         System.out.println("Pair 2: " + pair2.toString());
